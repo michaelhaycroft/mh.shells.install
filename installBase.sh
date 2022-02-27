4c916fbd8b34186c5c5c6cd664b76e38b2d6a0d7
michaelhaycroft <44305759+michaelhaycroft@users.noreply.github.com>
2022-02-27 12:54:44 +1100

    Update installBase.sh

--- installBase.sh
+++ installBase.sh
@@ -1,5 +1,19 @@
-SHELLS_INSTALL_ROOT="/mh.shells" # INSERT THE TARGET PATH FOR INSTALLATION HERE
-INSTALL_PACKAGES="1"
+REPOSITORY_CLONE_URL="git@github.com:michaelhaycroft/mh.shells.git"
+DEFAULT_SHELLS_INSTALL_ROOT="/mh.shells"
+SHELLS_INSTALL_ROOT="${1:-$DEFAULT_SHELLS_INSTALL_ROOT}"
+INSTALL_PACKAGE_MANAGERS="${2:-1}"
+INSTALL_PACKAGES="${3:-1}"
+INSTALL_CONFIGURATIONS="${3:-1}"
+INSTALLER_PARAMETERS=""
+if [[ "$INSTALL_PACKAGE_MANAGERS" == "1" ]]; then
+    INSTALLER_PARAMETERS="${INSTALLER_PARAMETERS} -p"
+fi
+if [[ "$INSTALL_PACKAGES" != "1" ]]; then
+    INSTALLER_PARAMETERS="${INSTALLER_PARAMETERS} -i NONE"
+fi
+if [[ "$INSTALL_PACKAGES" != "1" ]]; then
+    INSTALLER_PARAMETERS="${INSTALLER_PARAMETERS} -c NONE"
+fi
 GIT_INSTALL_ERROR="0"
 [[ ! -z "$(which git)" ]] || sudo dnf install -y git-all || sudo apt install -y git-all || GIT_INSTALL_ERROR="1"
 if [[ "$GIT_INSTALL_ERROR" == "1" ]]; then
@@ -10,18 +24,18 @@ else
   GITHUB_SSH_KEY_OUTFILE="$HOME/.ssh/github_$(echo $GITHUB_USER_EMAIL | sed 's/\./dot/g' | sed 's/@/at/g')"
   ssh-keygen -t ed25519 -C $GITHUB_USER_EMAIL -f $GITHUB_SSH_KEY_OUTFILE < <(echo -e "$GITHUB_SSH_PASSPHRASE\n$GITHUB_SSH_PASSPHRASE\n")
   cat $GITHUB_SSH_KEY_OUTFILE.pub
-  echo "UPLOAD THE ABOVE GENERATED SSH PUBLIC KEY ($GITHUB_SSH_KEY_OUTFILE) TO GITLAB THEN PRESS ANY KEY TO CONTINUE"
+  echo "UPLOAD THE ABOVE GENERATED SSH PUBLIC KEY ($GITHUB_SSH_KEY_OUTFILE) TO GITHUB THEN PRESS ANY KEY TO CONTINUE"
   read
   PARENT_DIR="$(dirname "$SHELLS_INSTALL_ROOT")"
   sudo mkdir -p "$PARENT_DIR"
   sudo chown $USER "$PARENT_DIR"
   pushd $PARENT_DIR
-  git clone git@github.com:michaelhaycroft/mh.shells.git
+  git clone "$REPOSITORY_CLONE_URL"
   popd
   sudo chown $USER $SHELLS_INSTALL_ROOT -R
   sudo chmod u+x $SHELLS_INSTALL_ROOT -R
   if [[ "$INSTALL_PACKAGES" == "1" ]]; then
     [[ ! -z "$(which datamash)" ]] || sudo dnf install datamash || sudo apt install datamash || echo "Error: failed to install datamash" # The install framework requires datamash
-    bash "$SHELLS_INSTALL_ROOT/linux/scripts/install.sh"
+    bash "$SHELLS_INSTALL_ROOT/linux/scripts/install.sh" "$INSTALLER_PARAMETERS"
   fi
 fi
