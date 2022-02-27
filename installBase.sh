d674ba45454149169b48dc247c72fa34b2e68bb1
michaelhaycroft <44305759+michaelhaycroft@users.noreply.github.com>
2022-02-27 13:46:08 +1100

    Update installBase.sh

--- installBase.sh
+++ installBase.sh
@@ -26,27 +26,50 @@ if [[ "$INSTALL_CONFIGURATIONS" != "1" ]]; then
     INVOKE_INSTALLER="1"
 fi
 GIT_INSTALL_ERROR="0"
-[[ ! -z "$(which git)" ]] || sudo dnf install -y git-all || sudo apt install -y git-all || GIT_INSTALL_ERROR="1"
-if [[ "$GIT_INSTALL_ERROR" == "1" ]]; then
-  echo "Install Git using a package manager available on your system and run again."
-else
-  read -p "Enter a passphrase for Github SSH (e.g. empty string): " GITHUB_SSH_PASSPHRASE
-  read -p "Enter github user email: " GITHUB_USER_EMAIL
-  GITHUB_SSH_KEY_OUTFILE="$HOME/.ssh/github_$(echo $GITHUB_USER_EMAIL | sed 's/\./dot/g' | sed 's/@/at/g')"
-  ssh-keygen -t ed25519 -C $GITHUB_USER_EMAIL -f $GITHUB_SSH_KEY_OUTFILE < <(echo -e "$GITHUB_SSH_PASSPHRASE\n$GITHUB_SSH_PASSPHRASE\n")
-  cat $GITHUB_SSH_KEY_OUTFILE.pub
-  echo "UPLOAD THE ABOVE GENERATED SSH PUBLIC KEY ($GITHUB_SSH_KEY_OUTFILE) TO GITHUB THEN PRESS ANY KEY TO CONTINUE"
-  read
-  PARENT_DIR="$(dirname "$SHELLS_INSTALL_ROOT")"
-  sudo mkdir -p "$PARENT_DIR"
-  sudo chown $USER "$PARENT_DIR"
-  pushd $PARENT_DIR
-  git clone "$REPOSITORY_CLONE_URL"
-  popd
-  sudo chown $USER $SHELLS_INSTALL_ROOT -R
-  sudo chmod u+x $SHELLS_INSTALL_ROOT -R
-  if [[ "$INVOKE_INSTALLER" == "1" ]]; then
-    [[ ! -z "$(which datamash)" ]] || sudo dnf install datamash || sudo apt install datamash || echo "Error: failed to install datamash" # The install framework requires datamash
-    bash "$SHELLS_INSTALL_ROOT/linux/scripts/install.sh" "$INSTALLER_PARAMETERS"
-  fi
+
+function InstallPackageOrFailAndExit() {
+    local ExecutableName="$1"
+    local PackageName="${2:-$ExecutableName}"
+    echo "Installing $ExecutableName if not already installed"
+    if [[ -z "$(which "$ExecutableName")" ]]; then
+        sudo dnf install -y "$PackageName" || \
+        sudo apt-get install -y "$PackageName" || \
+        echo "Install of $ExecutableName failed" && \
+        exit
+        echo "$ExecutableName was installed"
+    else
+        echo "$ExecutableName is already installed"
+    fi
+    return 0
+}
+
+function SetupSshAccessToRepository() {
+    read -p "Enter a passphrase for Github SSH (e.g. empty string): " GITHUB_SSH_PASSPHRASE
+    read -p "Enter github user email: " GITHUB_USER_EMAIL
+    GITHUB_SSH_KEY_OUTFILE="$HOME/.ssh/github_$(echo $GITHUB_USER_EMAIL | sed 's/\./dot/g' | sed 's/@/at/g')"
+    ssh-keygen -t ed25519 -C $GITHUB_USER_EMAIL -f $GITHUB_SSH_KEY_OUTFILE < <(echo -e "$GITHUB_SSH_PASSPHRASE\n$GITHUB_SSH_PASSPHRASE\n")
+    cat $GITHUB_SSH_KEY_OUTFILE.pub
+    echo "UPLOAD THE ABOVE GENERATED SSH PUBLIC KEY ($GITHUB_SSH_KEY_OUTFILE) TO GITHUB THEN PRESS ANY KEY TO CONTINUE"
+    read
+}
+
+function CloneShellsRepository() {
+    PARENT_DIR="$(dirname "$SHELLS_INSTALL_ROOT")"
+    sudo mkdir -p "$PARENT_DIR"
+    sudo chown $USER "$PARENT_DIR"
+    pushd $PARENT_DIR
+    git clone "$REPOSITORY_CLONE_URL"
+    popd
+    sudo chown $USER $SHELLS_INSTALL_ROOT -R
+    sudo chmod u+x $SHELLS_INSTALL_ROOT -R
+}
+
+InstallPackageOrFailAndExit "git" "git-all"
+InstallPackageOrFailAndExit "ssh-keygen" "x"
+SetupSshAccessToRepository
+CloneShellsRepository
+if [[ "$INVOKE_INSTALLER" == "1" ]]; then
+    InstallPackageOrFailAndExit "datamash"
+    echo "Launching package installer"
+    sudo bash "$SHELLS_INSTALL_ROOT/linux/scripts/install.sh" "$INSTALLER_PARAMETERS"
 fi
