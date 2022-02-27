a45ca99c02e4488918457007ccd7c4502ea9461d
michaelhaycroft <44305759+michaelhaycroft@users.noreply.github.com>
2022-02-27 18:09:58 +1100

    Update installBase.sh

--- installBase.sh
+++ installBase.sh
@@ -101,6 +101,5 @@ fi
 if [[ "$INVOKE_INSTALLER" == "1" ]]; then
     InstallPackageOrFailAndExit "datamash"
     echo "Launching package installer"
-    #bash "$SHELLS_INSTALL_ROOT/linux/scripts/install.sh" $INSTALLER_PARAMETERS
-    echo "bash \"$SHELLS_INSTALL_ROOT/linux/scripts/install.sh\" $INSTALLER_PARAMETERS"
+    bash "$SHELLS_INSTALL_ROOT/linux/scripts/install.sh" $INSTALLER_PARAMETERS
 fi
